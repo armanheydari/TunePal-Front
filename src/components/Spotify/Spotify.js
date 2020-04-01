520477f9ab8adb9a23a44f1c9396ce906873b1ce
ALi3TR <ali_sedaghi@ymail.com>
2020-04-01 06:33:43 +0430

    Getting Spotify account completed.

--- src/components/Spotify/Spotify.js
+++ src/components/Spotify/Spotify.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import axios from 'axios';
-
-const config = {
-    headers: {
-        'Authorization': `Token ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json'
-    }
-}
+import './styles/Spotify.css';
 
 class Spotify extends React.Component {
     state = {
-
     }
 
     onClick = () => {
-        console.log(localStorage.getItem('token'));
+        const config = {
+            headers: {
+                'Authorization': `Token ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            }
+        }
         axios.get('http://tunepal.pythonanywhere.com/spotify/auth/', config)
         .then(res => {
-
-            window.open(res.data.spotifyurl, '_blank');
             console.log(res);
+            window.open(res.data.spotifyurl, '_self');
         })
+        .catch(err => {
+            //if the user wasn't able to connect to sptify
+        });
     }
 
     render() {
         return (
-            <button onClick={this.onClick}>Link To Spotify</button>
+            <div>
+                <p>{this.props.name}</p>
+                <button onClick={this.onClick}>Link To Spotify</button>
+            </div>
         );
     }
 }
---
+++ src/components/Spotify/SpotifyResult.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class SpotifyResult extends React.Component {
+    state = {
+        show: false,
+        succeed: false
+    }
+    componentDidMount() {
+        const code = window.location.href.split("spotifyresult/")[1];
+        const serverURL = `http://tunepal.pythonanywhere.com/spotify/get_url/${code}`;
+        const config = {
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${localStorage.getItem('token')}`
+            }
+        }
+        axios.get(serverURL, config)
+        .then(res => {
+            console.log(res);
+            this.setState(() => {
+                return {
+                    show: true,
+                    succeed: true
+                };
+            });
+        })
+        .catch(err => {
+            this.setState(() => {
+                return {
+                    show: true,
+                };
+            });
+        });
+    }
+    render() {
+        if (!this.state.show) {
+            return null;
+        }
+        if (this.state.succeed) {
+            return (
+                <div>
+                    <p>{this.props.name}</p>
+                    <p>TunePal + Spotify = Done</p>
+                    <Link to="/">Continue to Homepage.</Link>
+                </div>
+            );
+        }
+        return (
+            <div>
+                <p>{this.props.name}</p>
+                <p>There was a problem connecting your Spotify account to TunePal.</p>
+                <p>You can connect it any time in Setting.</p>
+                <Link to="/">Continue to Homepage.</Link>
+            </div>
+        );
+    }
+}
+
+export default SpotifyResult;
