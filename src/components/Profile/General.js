0ada6019b583293da707311b41fd221594741d2f
ALi3TR <ali_sedaghi@ymail.com>
2020-04-03 08:06:52 +0430

    Profile finalized.

--- src/components/Profile/General.js
+++ src/components/Profile/General.js
@@ -1,75 +1,53 @@
 import React from 'react';
-import './styles/General.css';
 import ProfilePicture from '../../assets/maxresdefault.jpg';
 
 class General extends React.Component {
     state = {
-        //Profile Picture
-        name: 'Fati Komando',
-        status: 'Online',
-        age: '20',
-        gender: 'Female',
-        location: 'Iran, Tehran, Navvab',
-        username: '@ftikmnd',
-        bio: "Lorem ipsum dolor sit ameolor sit amet Lorem ipsumum Lorem ipsum dolor.",
-        //Lorem ipsum dolor sit ameolor sit amet Lorem ipsum Lorem ipsum dolor sit ameolor sit amet Lorem ipsum Lorem ipsum dolor.
-        favorites: '#Music #TV #biglittlelies'
+    }
+
+    location = () => {
+        return `${this.props.user.country} , ${this.props.user.province} , ${this.props.user.neighbourhood}`
     }
 
     render() {
         return (
             <div className="Profile_General_container">
-                <div className="row">
 
-                    <div className="col-lg-3 col-md-6 col-sm-12 Profile_General_image-container">
-                        {/* <div className="Profile_General_status">{this.state.status}</div> */}
-                        <img className="Profile_General_image" alt="profile-img" src={ProfilePicture}></img>
+                <div className="Profile_General_info-container">
+                    <h2>General Info</h2>
+                    <div className="Profile_General_info-field">
+                        <span className="Profile_General_info-label">Name</span>
+                        <span>{this.props.user.name}</span>
                     </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-12 Profile_General_info-container">
-                        <table className="Profile_General_table">
-                            <tbody>
-                                <tr className="Profile_Genral_table-row">
-                                    <th className="Profile_Genral_table-head">Name</th>
-                                    <td className="Profile_Genral_table-data">{this.state.name}</td>
-                                </tr>
-
-                                <tr className="Profile_Genral_table-row">
-                                    <th className="Profile_Genral_table-head">Gender</th>
-                                    <td className="Profile_Genral_table-data">{this.state.gender}</td>
-                                </tr>
-
-                                <tr className="Profile_Genral_table-row">
-                                    <th className="Profile_Genral_table-head">Age</th>
-                                    <td className="Profile_Genral_table-data">{this.state.age}</td>
-                                </tr>
-
-                                <tr className="Profile_Genral_table-row">
-                                    <th className="Profile_Genral_table-head">Location</th>
-                                    <td className="Profile_Genral_table-data">{this.state.location}</td>
-                                </tr>
-
-                                <tr className="Profile_Genral_table-row">
-                                    <th className="Profile_Genral_table-head">Username</th>
-                                    <td className="Profile_Genral_table-data">{this.state.username}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div className="Profile_General_info-field">
+                        <span className="Profile_General_info-label">Gender</span>
+                        <span>{this.props.user.gender}</span>
                     </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-12 Profile_General_bio-container">
-                        <div className="Profile_General_bio">
-                            <label className="Profile_General_label">Biography</label>
-                            {this.state.bio}
-                        </div>
-
+                    <div className="Profile_General_info-field">
+                        <span className="Profile_General_info-label">Birthday</span>
+                        <span>{this.props.user.birthday}</span>
                     </div>
+                    <div className="Profile_General_info-field">
+                        <span className="Profile_General_info-label">Location</span>
+                        <span>{this.location()}</span>
+                    </div>
+                    <div className="Profile_General_info-field">
+                        <span className="Profile_General_info-label">Username</span>
+                        <span>{this.props.user.username}</span>
+                    </div>
+                </div>
 
-                    <div className="col-lg-3 col-md-6 col-sm-12 Profile_General_fav-container">
-                        <div className="Profile_General_fav">
-                            <label className="Profile_General_label">Favourite</label>
-                            {this.state.favorites}
-                        </div>
+                <div className="Profile_General_bio-container">
+                    <h2>Biography</h2>
+                    <div className="Profile_General_bio">
+                        {this.props.user.bio}
+                    </div>
+                </div>
+
+                <div className="Profile_General_fav-container">
+                    <h2>Favourites</h2>
+                    <div className="Profile_General_fav">
+                        {this.props.user.favorites}
                     </div>
                 </div>
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import General from './General';
+import SongTable from './SongTable';
+import ArtistTable from './ArtistTable';
+import AlbumTable from './AlbumTable';
+import GenreTable from './GenreTable';
 
 class Profile extends React.Component {
     state = {
@@ -8,16 +12,19 @@ class Profile extends React.Component {
 
     render() {
         return (
-            <React.Fragment>
-                <General />
-                <div className="Profile_tables-container">
+            <div className="Profile_container">
+                <h1 className="Profile_title">Profile</h1>
+                <General user={this.props.user} />
+                {/* <div className="Profile_tables-container">
                     <div className="row">
                         <div className="col-lg-6 col-md-12 col-sm-12">
                             <h3>Song</h3>
+                            <SongTable />
                         </div>
 
                         <div className="col-lg-6 col-md-12 col-sm-12">
                             <h3>Artist</h3>
+                            <ArtistTable />
                         </div>
 
                     </div>
@@ -25,15 +32,17 @@ class Profile extends React.Component {
                     <div className="row">
                         <div className="col-lg-6 col-md-12 col-sm-12">
                             <h3>Album</h3>
+                            <AlbumTable />
                         </div>
 
                         <div className="col-lg-6 col-md-12 col-sm-12">
                             <h3>Genre</h3>
+                            <GenreTable />
                         </div>
 
                     </div>
-                </div>
-            </React.Fragment>
+                </div> */}
+            </div>
         );
     };
 }
