9e7ceabc2594fdcbd5e5c9f1275e11e5f1f83027
ALi3TR <ali_sedaghi@ymail.com>
2020-04-03 08:04:15 +0430

    Layout finalized

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import AppLogo from '../../assets/app-logo.png';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className="Header_container">
-            <div className="Header_title">TunePal</div>
-            <div className="Header_description">Music Based Dating Web App</div>
+            <div className="Header-app-info-container">
+                <img className="Header_logo" alt="app-logo-img" src={AppLogo}></img>
+                <div>
+                    <h1 className="Header_title">TunePal</h1>
+                    <h2 className="Header_subtitle">Music + Dating</h2>
+                </div>
+            </div>
+            <p className="Header_name">{props.name}</p>
         </header>
     );
 }
--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles/w3.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faHeart, faAddressCard, faUserCog, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import ProfilePicture from '../../assets/maxresdefault.jpg';
 
-function Sidebar() {
+function Sidebar(props) {
     return (   
-        <div className="Sidebar_container w3-sidebar w3-bar-block w3-black w3-xxlarge">
-            <Link to="/" className="w3-bar-item w3-button"><FontAwesomeIcon icon={faHome} /></Link> 
-            <Link to="/match" className="w3-bar-item w3-button"><FontAwesomeIcon icon={faHeart} /></Link> 
-            <Link to="/profile" className="w3-bar-item w3-button"><FontAwesomeIcon icon={faAddressCard} /></Link> 
-            <Link to="/setting" className="w3-bar-item w3-button"><FontAwesomeIcon icon={faUserCog} /></Link>
-            <Link to="/logout" className="w3-bar-item w3-button"><FontAwesomeIcon icon={faPowerOff} /></Link> 
+        <div className="Sidebar_container w3-sidebar w3-bar-block w3-xxlarge">
+            <div className="w3-bar-item Sidebar_photo-container"><img className="Sidebar_photo" alt="profile-img" src={ProfilePicture}></img></div>
+            <Link to="/" className="w3-bar-item w3-button Sidebar_item"><FontAwesomeIcon icon={faHome} /><span className="Sidebar_item-name">Home</span></Link> 
+            <Link to="/match" className="w3-bar-item w3-button Sidebar_item"><FontAwesomeIcon icon={faHeart} /><span className="Sidebar_item-name">Match</span></Link> 
+            <Link to="/profile" className="w3-bar-item w3-button Sidebar_item"><FontAwesomeIcon icon={faAddressCard} /><span className="Sidebar_item-name">Profile</span></Link> 
+            <Link to="/setting" className="w3-bar-item w3-button Sidebar_item"><FontAwesomeIcon icon={faUserCog} /><span className="Sidebar_item-name">Setting</span></Link>
+            <button className="w3-bar-item w3-button Sidebar_item" onClick={props.logout}><FontAwesomeIcon icon={faPowerOff} /><span className="Sidebar_item-name">Logout</span></button>
         </div>
         
     );
