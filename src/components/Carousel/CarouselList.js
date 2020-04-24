5a8d03c9cce9b49a7092744039d96b8bfa56ba62
ALi3TR <ali_sedaghi@ymail.com>
2020-04-24 05:06:56 +0430

    [chore] Remove css import in Carousel (Imported in css container)

--- src/components/Carousel/CarouselList.js
+++ src/components/Carousel/CarouselList.js
@@ -3,7 +3,6 @@ import CarouselItem from './CarouselItem';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import './styles/Carousel.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
