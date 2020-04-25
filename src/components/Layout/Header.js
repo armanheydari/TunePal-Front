d39c12c3344f0c6d3a3928fa8cf70477d59c463f
ALi3TR <ali_sedaghi@ymail.com>
2020-04-26 02:09:23 +0430

    [feat] Make sidebar responsive and improve layout

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import AppLogo from '../../assets/app-logo.png';
 
-const Header = (props) => {
-    return (
-        <header className="Header_container">
-            <div className="Header-app-info-container">
-                <img className="Header_logo" alt="app-logo-img" src={AppLogo}></img>
-                <div>
-                    <h1 className="Header_title">TunePal</h1>
-                    <h2 className="Header_subtitle">Music + Dating</h2>
+const makeSidebarOn = () => {
+    const sidebarOverlay = document.getElementById("SidebarOverlay");
+    sidebarOverlay.style.display = "block";
+    const headerToggler = document.getElementById("Header_toggle");
+    headerToggler.style.display = "none";
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="Header">
+                <div id="Header_toggle" className="Header_toggle" onClick={makeSidebarOn}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
-            </div>
-            <p className="Header_name">{props.name}</p>
-        </header>
-    );
+                <div className="Header_logo">
+                    <img className="Header_logo-img" alt="" src={AppLogo} />
+                    <div>
+                        <h1 className="Header_title">TunePal</h1>
+                        <h2 className="Header_subtitle">Music + Dating</h2>
+                    </div>
+                </div>
+                <p className="Header_name">{this.props.name}</p>
+            </header>
+        );
+    }
 }
 
 export default Header;
