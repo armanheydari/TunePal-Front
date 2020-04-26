1895275d43db7a4cd87deb8d596d12308c755ef5
ALi3TR <ali_sedaghi@ymail.com>
2020-04-26 05:34:46 +0430

    [chore] Import ant-design css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import 'antd/dist/antd.css';
 import './style-container.scss';
 import Main from './components/Main.js';
 import * as serviceWorker from "./serviceWorker";
