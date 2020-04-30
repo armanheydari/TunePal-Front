8437653f3ae5730f33a5b6a70dbed4df04da617d
ALi3TR <ali_sedaghi@ymail.com>
2020-04-30 06:43:10 +0430

    [refactor] Add no chat selected state

--- src/components/Chat/ChatBox/ChatBox.js
+++ src/components/Chat/ChatBox/ChatBox.js
@@ -5,15 +5,24 @@ import ChatSend from './ChatSend';
 
 class ChatBox extends React.Component {
     render() {
+        if (Object.keys(this.props.header).length) {
+            return (
+                <div className="chat">
+                    <ChatHeader
+                        picture={this.props.header.picture}
+                        name={this.props.header.name}
+                        description="Description"
+                    />
+                    <ChatHistory messages={this.props.messages} />
+                    <ChatSend send={this.props.send} />
+                </div>
+            );
+        }
         return (
             <div className="chat">
-                <ChatHeader
-                    picture={this.props.header.picture}
-                    name={this.props.header.name}
-                    description="Description"
-                />
-                <ChatHistory messages={this.props.messages} />
-                <ChatSend send={this.props.send} />
+                <div className="Chat_noChatSelected">
+                    <p>Please select a chat to start messaging.</p>
+                </div>
             </div>
         );
     }
