cf014a1a4eaee1b5ab3191e4fd914624b308137f
sabaroohandeh <sabaroohandeh@users.noreply.github.com>
2020-05-01 20:22:45 +0430

    finish landingPage

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,6 +13,8 @@ import Match from './Match/Match';
 import Quiz from './Quiz/Quiz.js';
 import Chat from './Chat/Chat';
 import SidebarOverlay from './Layout/SidebarOverlay';
+import LandingPage from './LandingPage/LandingPage.js';
+//import LandingPage from './LandingPage/LandingPage.js';
 
 const tokenConfig = () => {
     return {
@@ -70,6 +72,7 @@ class Main extends React.Component {
         if (this.state.isLoggedIn) {
             return (
                 <Router>
+                    
                     <Header username={this.state.username} />
                     <div className="Main_row">
                         <div className="Main_sidebar">
@@ -120,8 +123,13 @@ class Main extends React.Component {
         else {
             return (
                 <Router>
-                    <Route exact path="/"><LoginSignup setUserInfo={this.setUserInfo} /></Route>
+                    <Switch>
+                    
+                    <Route exact path="/" component = {LandingPage }></Route>
+                   <Route  path="/LoginSignup" component = {LoginSignup }><LoginSignup setUserInfo={this.setUserInfo}/></Route>
+                        
                     <Redirect from='*' to='/' />
+                    </Switch>
                 </Router>
             );
         }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import 'normalize.css/normalize.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'antd/dist/antd.css';
 import './style-container.scss';
-import Main from './components/Main.js';
+ import Main from './components/Main.js';
 import * as serviceWorker from "./serviceWorker";
+import LandingPage from './components/LandingPage/LandingPage.js';
 
 ReactDOM.render(<Main />, document.getElementById("root"));
 
