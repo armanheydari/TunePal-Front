d78c442588e89b62b1949629b90b4f24a4c52edf
arman <armanalaki@gmail.com>
2020-05-09 20:23:47 +0430

    [feat]Add play button on hover

--- src/components/Homepage/Carousel/CarouselItem.js
+++ src/components/Homepage/Carousel/CarouselItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class CarouselItem extends React.Component {
+    state = {
+        play: false
+    }
     render() {
         const { index, title, subtitle, imgURL } = this.props;
         return (
-            <div className="Carousel_item" onClick={this.openSpotify} >
+            <div className="Carousel_item" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
                 <div className="Carousel_pic">
-                    <img className="Carousel_img" src={imgURL} alt="" />
+                    {(this.state.play == false)
+                        ?<img className="Carousel_img" src={imgURL} alt="" />
+                        :<div style={{width:"100%",height:"20rem"}}>
+                        <img className="Carousel_imgp" src={imgURL} alt="" />
+                        <button className="Carousel_play-button" onClick={this.openSpotify}><i className="play icon"/></button>
+                        </div>
+                    }
                 </div>
                 <div className="Carousel_item-text">
                     <p>{index + 1}: {title}</p>
@@ -16,8 +25,24 @@ class CarouselItem extends React.Component {
         );
     }
 
+    mouseEnter = () => {
+        this.setState(() => {
+            return {
+                play: true
+            };
+        });
+    }
+
+    mouseLeave = () => {
+        this.setState(() => {
+            return {
+                play: false
+            };
+        });
+    }
+
     openSpotify = () => {
-        window.open(`${this.props.spotifyURL}`,'_blank');
+        window.open(`${this.props.spotifyURL}`, '_blank');
     }
 }
 
