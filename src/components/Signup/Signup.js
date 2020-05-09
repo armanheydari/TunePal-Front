0f1f7e4119cd0593ca23230e72e6582a71ced087
ALi3TR <ali_sedaghi@ymail.com>
2020-05-09 04:59:34 +0430

    [feat] Add wrapper for after signup procedure

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './styles/Signup.css';
+import SignupSteps from './SignupSteps';
+import Interest from './Interest';
+import Location from './Location';
+import Spotify from './Spotify';
+import Pagination from './Pagination';
+
+class Signup extends React.Component {
+    state = {
+        stage: "favorites",
+        favorites: [],
+        location: undefined
+    }
+
+    render() {
+        const stage = this.state.stage;
+        return (
+            <div className="Signup">
+                <SignupSteps stage={stage} />
+                {
+                    stage === "favorites" &&
+                    <Interest
+                        updateStage={this.updateStage}
+                        favorites={this.state.favorites}
+                        updateFavorites={this.updateFavorites}
+                    />
+                }
+
+                {
+                    stage === "location" &&
+                    <Location
+                        updateStage={this.updateStage}
+                        location={this.state.location}
+                        updateLocation={this.updateLocation}
+                    />
+                }
+                {
+                    stage === "spotify" &&
+                    <Spotify
+                        updateStage={this.updateStage}
+                    />
+                }
+            </div>
+        );
+    }
+
+    updateStage = (stage) => {
+        this.setState(prevState => {
+            return {
+                stage
+            };
+        });
+    }
+
+    updateFavorites = (favorites) => {
+        this.setState(prevState => {
+            return {
+                favorites
+            };
+        });
+    }
+
+    updateLocation = (location) => {
+        this.setState(prevState => {
+            return {
+                location
+            };
+        });
+    }
+}
+
+export default Signup;
