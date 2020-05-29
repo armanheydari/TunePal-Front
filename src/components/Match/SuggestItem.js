d0882f2d359858430f5175f989cc0392945b5190
ALi3TR <ali_sedaghi@ymail.com>
2020-05-30 03:11:36 +0430

    [fix] Add load state on sending requests

--- src/components/Match/SuggestItem.js
+++ src/components/Match/SuggestItem.js
@@ -4,8 +4,13 @@ import Axios from 'axios';
 import { Link } from 'react-router-dom';
 import tokenConfig from '../../utils/tokenConfig';
 import serverURL from '../../utils/serverURL';
+import { Button } from 'antd';
 
 class SuggestItem extends React.Component {
+  state = {
+    isSending: undefined
+  }
+
   render() {
     const { name, age, distance, imgURL, pending } = this.props;
     return (
@@ -34,18 +39,36 @@ class SuggestItem extends React.Component {
         {
           pending 
           ? <div className="pending">Pending</div> 
-          : <div onClick={this.onClickRequest} className="matchList-firstList_list__request">Request</div>
+          // : <div onClick={this.onClickRequest} style={this.state.isSending ? {pointerEvents:'none'} : {pointerEvents:'none'}} className="matchList-firstList_list__request">Request</div>
+          : <Button loading={this.state.isSending} className="matchList-firstList_list__request" onClick={this.onClickRequest}>
+              Request
+          </Button>
         }
       </li>
     );
   }
 
   onClickRequest = () => {
+    this.setState(prevState => {
+      return {
+        isSending: true
+      };
+    });
     Axios.get(`${serverURL()}/spotify/friend_request/?username=${this.props.username}`, tokenConfig())
     .then(res => {
+      this.setState(prevState => {
+        return {
+          isSending: false
+        };
+      });
       this.props.updatePending(this.props.username);
     })
     .catch(err => {
+      this.setState(prevState => {
+        return {
+          isSending: false
+        };
+      });
     });
   }
 
